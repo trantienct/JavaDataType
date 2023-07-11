a28057a41adfc868e4988c25c87e76c6a7132d1f
CT_TienTV <tientv@tora-tech.com>
2023-07-11 10:49:00 +0700

    Exercises 11/07/2023

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Exercise.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author trant
+ */
+public class Exercise {
+    /* 
+    Bài tập 1:
+    - Xem ví dụ về cách nhập dữ liệu từ bàn phím trong file Main.java
+    - Viết chương trình nhập vào độ C, sau đó in ra kết quả là độ F
+    - Công thức chuyển: độ F = ( 1.8 * độ C ) + 32
+    - Ví dụ: Hãy nhập độ C: 37
+    - Kết quả mong muốn (In ra màn hình): 37 độ C tương ứng với 98.6 độ F
+    */
+    
+    /* 
+    Bài tập 2: Viết chương trình cho phép nhập vào phút, và in ra màn hình năm và ngày
+    - Ví dụ: Hãy nhập số phút: 1440
+    - Kết quả mong muốn: 1440 phút tương đương với 0 năm 1 ngày
+    */
+    
+    /* 
+    Bài tập 3: Viết chương trình tính toán chỉ số cơ thể BMI, với các giá trị nhập vào là height (inches) và weight (pounds)
+    Kết quả trả về là chỉ số BMI
+    - Ví dụ: Your height (inches): 72
+             Your weight (pounds): 160
+    - Kết quả mong muốn: Your BMI is 21.7
+    */
+}
