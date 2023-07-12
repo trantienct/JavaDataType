f0dfb6b293c1ebfe2c7e739745c84949893ea701
CT_TienTV <tientv@tora-tech.com>
2023-07-12 13:36:36 +0700

    Ví dụ bài tập 1

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Main.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         // Khởi tạo 1 đối tượng Scanner có tên là input. System.in là tham số đầu vào mặc định (Bàn phím)
         Scanner input = new Scanner(System.in);
-        System.out.println("Your number: ");
+        System.out.println("Hãy nhập độ C: ");
         // Khởi tạo giá trị nhập vào kiểu int
         // float number = input.nextFloat() - Kiểu dữ liệu float
         // long number = input.nextLong() - Kiểu dữ liệu long
@@ -23,8 +23,20 @@ public class Main {
         // String name = input.nextLine() - Kiểu dữ liệu string
         /* String name = input.next() - Kiểu dữ liệu string 
         Đọc tới ký tự khoảng trắng thì dừng, in ra màn hình. Ví dụ tran van tien -> kết quả trả về của name là tran */
-        int number = input.nextInt();
-        // In ra kết quả vừa nhập
-        System.out.println("Your input number is: " + number);
+        double number = input.nextInt();
+//        // In ra kết quả vừa nhập
+//        System.out.println("Your input number is: " + number);
+        /*
+        Bài tập 1:
+        - Xem ví dụ về cách nhập dữ liệu từ bàn phím trong file Main.java
+        - Viết chương trình nhập vào độ C, sau đó in ra kết quả là độ F
+        - Công thức chuyển: độ F = ( 1.8 * độ C ) + 32
+        - Ví dụ: Hãy nhập độ C: 37
+        - Kết quả mong muốn (In ra màn hình): 37 độ C tương ứng với 98.6 độ F
+        */
+        double fDegree = (1.8 * number) + 32;
+        System.out.println( number + " độ C tương ứng với " + String.format("%.2f", fDegree) + " độ F");
+        // Tương tự cho các bài tập tiếp theo
+        /*  Bài tập 2 */
     }
 }
