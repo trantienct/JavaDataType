5cb02d6202f2ba29fc4601e0448ea557078ffdbf
CT_TienTV <tientv@tora-tech.com>
2023-07-14 08:44:25 +0700

    Bài tập ngày 14/07/2023: Các bài tập số 7, 8, 9, 10, 11, 12 về java cơ bản - Vòng lặp và điều kiện

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Exercise.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Exercise.java
@@ -9,27 +9,67 @@ package com.mycompany.mavenproject1;
  * @author trant
  */
 public class Exercise {
-    // Commit Message: Giải bài tập 4,5,6 về [kiểu dữ liệu - DataType] int, char, double, float
+    // Commit Message: Bài tập về các toán tử, vòng lặp và điều kiện
     /*
-    * Bài tập 4: Viết chương trình, cho phép người dùng nhập vào 3 ký tự, sau đó hiển thị đảo ngược các ký tự
-    * Ví dụ: Nhập ký tự: A
-    * Nhập ký tự: B
-    * Nhập ký tự: C
-    * Hiển thị ra màn hình: C B A
+    * Bài tập 7: Viết chương trình hoán đổi giá trị 2 biến số, yêu cầu không sử dụng biến thứ 3
+    * Ví dụ: Tạo 2 biến a, b
+    * a = 15
+    * b = 20
+    * Hiển thị ra màn hình:
+    * Giá trị trước khi hoán đổi của biến a là 15, biến b là 20
+    * Giá trị sau khi hoán đổi của biến a là 20, biến b là 15
     * */
     /*
-    Bài tập 5:
-    Viết chương trình cho phép người dùng nhập vào 2 giá trị số, và hiển thị các nội dung sau:
-    - Tổng của 2 số
-    - Hiệu của 2 số
-    - Tích của 2 số
-    - Giá trị trung bình của 2 số
-    - Số lớn nhất trong 2 số
-    - Số nhỏ nhất trong 2 số
+    Bài tập 8:
+    Viết chương trình lấy ngày hiện tại, sau đó hiển thị theo các định dạng:
+    - DD/MM/YYYY : 14/07/2023
+    - YYYY-MM-DD : 2023-07-14
+    - E, dd MMM yyyy HH:mm:ss : Fri, 14 Jul 2023 09:00:00
      */
     /*
-    Bài tập 6: Viết chương trình cho phép người dùng nhập vào 1 số, và in ra từng ký tự của số đó
-    - Ví dụ: Hãy nhập vào 1 số: 123456
-    - Kết quả mong muốn: 1 2 3 4 5 6
+    Bài tập 9: Viết chương trình kiểm tra số nhập vào là số chẵn hay số lẻ
+    - Ví dụ: Hãy nhập vào 1 số: 10
+    - Kết quả mong muốn: Số 10 là số chẵn
+     */
+    /*
+    Bài tập 10: Viết chương trình in ra các số lẻ từ số 0 đến số nhập vào từ bàn phím
+    - Ví dụ: Hãy nhập vào 1 số: 6
+    - Kết quả mong muốn:
+    Các số lẻ từ 0 đến 6 là:
+    1
+    3
+    5
+     */
+    /*
+    Bài tập 11: Viết chương trình nhập vào 1 số (kiểu double) và in ra giá trị trên màn hình theo các trường hợp:
+    - In ra Zero nếu nhập vào số 0
+    - Nếu số lớn hơn 0, thì in ra số dương
+    - Nếu số nhỏ hơn 0, thì in ra số âm
+    - Bỏ qua dấu, nếu nhỏ hơn 1 thì thêm vào chữ Nhỏ, lớn hơn 1 triệu thì thêm vào chữ Lớn
+    Ví dụ: Nhập vào 1 số: 1000.5
+    - Kết quả mong muốn: Số 1000 là số dương lớn
+    - Nhập vào 1 số: -0.4
+    - Kết quả mong muốn: Số -0.4 là số âm nhỏ
+     */
+    /*
+    Bài tập 12:
+    In ra 1 tháp các chữ số theo số hàng nhập vào từ bàn phím. Mỗi số sẽ cách nhau 1 đơn vị. Giải thích cách in ra kết quả
+    Ví dụ: Nhập vào số hàng: 5
+    Kết quả mong muốn:
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    11 12 13 14 15
+     */
+    /*
+    Bài tập 13:
+    In ra 1 kim tự tháp theo số hàng nhập vào từ bàn phím. Mỗi số sẽ cách nhau 1 đơn vị. Giải thích cách in ra kết quả
+    Ví dụ: Nhập vào số hàng: 4
+    Kết quả mong muốn:
+          1
+         2 2
+        3 3 3
+       4 4 4 4
      */
 }
