7be138af865fceec4ddca422c256b2bcb81fd56b
CT_TienTV <tientv@tora-tech.com>
2023-07-17 09:36:15 +0700

    Bài tập ngày 17/7: Method and Loop

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Exercise.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Exercise.java
@@ -11,33 +11,27 @@ import java.util.Scanner;
 
 
 public class Exercise {
-    // Commit Message: Bài tập về các toán tử, vòng lặp và điều kiện
+    // Commit Message: Bài tập về Method và Loop
    /*
-
-   Bài tập 17: Định nghĩa 1 method để tính tổng các số từ số nhập vào bàn phím
+   Bài tập 18: Định nghĩa 1 method để đảo ngược số nhập vào từ bàn phím
    - Ví dụ: Nhập vào 1 số: 12345
-   - Kết quả mong muốn: Tổng của số đó là 15;
-   - Định nghĩa 1 method static int sumDigitNumber(int number)
-   - Trong method main,
-   + Sử dụng scanner để nhập 1 giá trị từ bàn phím
-   + Đưa giá trị vào method sumDigitNumber
-   + Lấy kết quả trả về và in ra màn hình
+   - Kết quả mong muốn: Số đảo ngược của 12345 là 54321
     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Hãy nhập vào một số: ");
-        int number = scanner.nextInt();
-        int sum = sumDigitNumber(number);
-        System.out.println("Tổng của số đó là: " + sum);
-    }
+    /*
+   Bài tập 19: Xem các ví dụ trong file main.java về các cách sử dụng 2 vòng lặp while và do while
+   - Thực hiện từng Method doWhileWithInput và whileWithInput, sau đó cho chú biết
+   + Sự khác nhau của 2 method này
+     */
+    /*
+    Bài tập 20: Viết 1 Method cho phép người dùng nhập vào passcode và kiểm tra xem passcode có hợp lệ không
+    - Yêu cầu: Passcode là 1 number đã được thiết lập sẵn: int passcode = 998877
+    - Người dùng chỉ được nhập tối đa 3 lần, sau lần thứ 4 thì thoát khỏi chương trình. Nếu nhập đúng, in ra màn hình dòng chữ: Welcome back, user!
+    - Ví dụ:
+    + Please enter your passcode? 123456
+    + Wrong passcode. 2 attempts left. Please try again: 134567
+    + Wrong passcode. 1 attempts left. Please try again: 456789
+    + Wrong passcode. 0 attempts left. Please try again in 10 minutes.
+    Exit program
+     */
 
-    public static int sumDigitNumber(int number) {
-        int sum = 0;
-        while (number != 0) {
-            int digit = number % 10;
-            sum += digit;
-            number /= 10;
-        }
-        return sum;
-    }
 }
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Main.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -13,18 +13,79 @@ import java.util.Scanner;
 public class Main {
 /* Get input and  */
     public static void main(String[] args) {
-        // Khởi tạo 1 đối tượng Scanner có tên là input. System.in là tham số đầu vào mặc định (Bàn phím)
         Scanner input = new Scanner(System.in);
-        System.out.println("Your number: ");
-        // Khởi tạo giá trị nhập vào kiểu int
-        // float number = input.nextFloat() - Kiểu dữ liệu float
-        // long number = input.nextLong() - Kiểu dữ liệu long
-        // boolean number = input.nextBoolean() - Kiểu dữ liệu long
-        // String name = input.nextLine() - Kiểu dữ liệu string
-        /* String name = input.next() - Kiểu dữ liệu string 
-        Đọc tới ký tự khoảng trắng thì dừng, in ra màn hình. Ví dụ tran van tien -> kết quả trả về của name là tran */
-        int number = input.nextInt();
-        // In ra kết quả vừa nhập
-        System.out.println("Your input number is: " + number);
+       /*
+       Vòng lặp do while
+       - Sự khác biệt giữa do while và while
+       + While: Kiểm tra điều kiện thỏa mãn thì mới bắt đầu thực hiện các khối bên trong vòng lặp
+       + Do while: Thực hiện các khối trước 1 lần rồi mới kiểm tra tới điều kiện
+       + Về cơ  bản, while có thể có hoặc ko thực hiện lần lặp nào, trong khi do while sẽ thực hiện vòng lặp ít nhất 1 lần.
+       Cú pháp:
+       do{
+            //execute some codes
+       }while (condition)
+        */
+//        whileExample(); // Nothing is print
+//        doWhileExample(); // Print 10 then end
+//        int number = input.nextInt();
+//        // In ra kết quả vừa nhập
+//        System.out.println("Your input number is: " + number);
+    }
+
+    //While
+    static void whileExample(){
+        int i = 10;
+        while (i > 10){
+            System.out.println(i);
+            i--;
+        }
+    }
+    static void doWhileExample(){
+        int i = 10;
+        do{
+            System.out.println(i);
+            i--;
+        }while (i > 10);
+    }
+    /* Thao tác màn hình
+    Type your name: Bao
+    Your name is not correct. Do you want to try again - Y/N? Y
+    Type your name: Chuong
+    Welcome back, Chuong
+     */
+    static void doWhileWithInput(){
+        char choice;
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do{
+            System.out.print("Type your name: ");
+            input = scanner.next();
+            if(input.equals("Chuong")){
+                System.out.println("Welcome back, " + input);
+                choice = 'N';
+            }
+            else {
+                System.out.print("Your name is not correct. Do you want to try again - Y/N? ");
+                choice = scanner.next().charAt(0);
+            }
+        }while (choice == 'Y');
+    }
+
+    static void whileWithInput(){
+        char choice = 'Y';
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (choice == 'Y'){
+            System.out.print("Type your name: ");
+            input = scanner.next();
+            if(input.equals("Chuong")){
+                System.out.println("Welcome back, " + input);
+                choice = 'N';
+            }
+            else {
+                System.out.print("Your name is not correct. Do you want to try again - Y/N? ");
+                choice = scanner.next().charAt(0);
+            }
+        }
     }
 }
