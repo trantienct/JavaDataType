d0de9f22c5a155063edce9f3dbb9eb21acecd3fa
CT_TienTV <tientv@tora-tech.com>
2023-08-02 20:25:32 +0700

    Bài tập ngày 2/8: String and loop

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Exercise.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Exercise.java
@@ -11,41 +11,73 @@ import java.util.Scanner;
 
 
 public class Exercise {
-    // Commit Message: Bài tập về String
+    // Commit Message: Bài tập về vòng lặp, string
    /*
-   Bài tập 1:
-   - Nhập từ bàn phím 1 đoạn chữ, sau đó đảo ngược đoạn chữ đó
-   - Ví dụ: sample
-   - Kết quả mong muốn: elpmas
-   ############################################################
-   Bài tập 2:
-   - Nhập từ bàn phím 1 đoạn chữ, sau đó thay thế tất cả những ký tự a thành A
-   - Ví dụ: Nhập vào dòng chữ: This is a sample
-   - Kết quả mong muốn: This is A sAmple
-   #############################################################
-   Bài tập 3:
-   - Nhập vào 1 chuỗi
-   - Kiểm tra xem 1 chuỗi nào đó có thuộc chuỗi vừa nhập vào không. Nếu có, trả về True, sai thì trả về False
-   - Ví dụ: Hãy nhập vào 1 chuỗi: Java programming is really fun
-   - Bạn muốn kiểm tra chuỗi nào: fun
-   - Is 'fun' present in the text? (Hiển thị đoạn text kết hợp với input)
-   - True
-   ##############################################################
-   Bài tập 4:
-   - Nhập vào 1 chuỗi
-   - Kiểm tra xem chuỗi đó chỉ có chứa số hay không.
-   - Gợi ý: Sử dụng Character.isDigit() kết hợp vòng lặp để kiểm tra xem 1 ký tự trong chuỗi có phải là số hay không
-   Character.isDigit('a') -> false
-   Character.isDigit('1') -> true
-   - Ví dụ: Nhập vào 1 chuỗi: 123456123
-   - Chuỗi '1123456123' không có chứa ký tự
-   - Nhập vào 1 chuỗi: 123T123
-   - Chuỗi '123T123' có chứa ký tự
-   ##############################################################
-   Bài tập 5:
-   - Đếm số lần xuất hiện của 1 chuỗi trong chuỗi khác
-   Ví dụ: Có chuỗi sau: bb abb cc gg hh bab b b bba
-   - Chuỗi cần kiểm tra: bb
-   - Kết quả mong muốn: bb xuất hiện 2 lần trong chuỗi trên
+   Bài tập 1: Write a method to calculate factorial of number
+   Ví dụ: 5! = 1 * 2 * 3 * 4 * 5 = 120
+   Input: Type your number: 10
+   Output: Factorial of 10 is: 3628800
+   */
+    /*
+    Bài tập 2: Write a method that returns the nth element of the Fibonacci Sequence
+    Fibonacci: F(n)=F(n–1)+F(n–2)
+    Ví dụ: Dãy Fibonacci có cấu trúc như sau 0 1 1 2 3 5 8 13; tức là số thứ 1 + số thứ 2 = số thứ 3; số thứ 2 + số thứ 3 = số thứ 4 ....
+    Input: Fibonacci - number of elements: 10
+    Output: Your Fibonacci with 10 elements: 0 1 1 2 3 5 8 13 21 34
+     */
+    /*
+    Bài tập 3: Calculate Simple Interest and Compounding Interest by year, month
+    Compound Interest = P(1 + R/100)^t
+    P is principal amount, R is the rate and T is the time span (year); ^ for power: 2^2 = 2*2 = 4; 2^3 = 2*2*2 = 8
+     */
+    /*
+    Bài tập 4: Write a program that prompts user for an integer between 0-5 or 45-50.
+    The program  will repeat until the user enters a valid input.
+    Input:
+    Enter the number between 0-5 or 45-50: 10
+    Invalid number, try again...
+    Enter the number between 0-5 or 45-50: 20
+    Invalid number, try again...
+    Enter the number between 0-5 or 45-50: 3
+    You entered: 3
+    Exit
+     */
+    /*
+    Bài tập 5: Create a matrix table
+    Input: Enter size: 5
+    Output:
+     * |   1   2   3   4   5
+    ------------------------
+     1 |   1   2   3   4   5
+     2 |   2   4   6   8  10
+     3 |   3   6   9  12  15
+     4 |   4   8  12  16  20
+     5 |   5  10  15  20  25
+     */
+    /*
+    Bài tập 6: Caesar Cipher
+    The Caesar cipher is a simple encryption technique that was used by Julius Caesar to send secret messages to his allies.
+    It works by shifting the letters in the plaintext message by a certain number of positions, known as the "shift" or "key".
+    Promt user to type a string (mix-case letters) and return Caesar Cipher in uppercase
+    Input: Type your string: HeLLo
+    Assume key = 3 (Fixed in code)
+    Caesar Cipher: KHOOR
+     */
+    /*
+    Bài tập 7: Caesar Decipher
+    Input: Type your string: KHooR
+    Assume key = 3 (Fixed in code)
+    Caesar Decipher: HELLO
+     */
+    /*
+    Bài tập 8: Double char
+    Input: hello
+    Output: hheelllloo
+     */
+    /*
+    Bài tập 9:  Count the occurrences of a string (case sensitive).
+    Input: Input first string: Hello World
+    Input string to check: orld
+    Output: 'Hello World' contains 'orl'
      */
 }
